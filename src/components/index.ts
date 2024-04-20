238afbd7060e45640bf3684a6a1b814e81ed464c
Steven Bustillo <sb.sys.dev@gmail.com>
2024-04-20 19:26:49 +0000

    :lipstick: - added services design

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Icon from './Icon.astro';
 import Header from './Header.astro';
 import Welcome from './Welcome.astro';
+import Services from './Services.astro';
 
-export { Icon, Header, Welcome };
+export { Icon, Header, Welcome, Services };
