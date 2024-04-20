94cc2defa2dbfef94703e7942eabff2ebfa5bc40
Steven Bustillo <sb.sys.dev@gmail.com>
2024-04-20 20:53:07 +0000

    :lipstick: - added contacts design

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Icon from './Icon.astro';
 import Header from './Header.astro';
 import Welcome from './Welcome.astro';
 import Services from './Services.astro';
+import Contacts from './Contacts.astro';
 
-export { Icon, Header, Welcome, Services };
+export { Icon, Header, Welcome, Services, Contacts };
