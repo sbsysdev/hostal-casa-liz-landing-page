bdb849cc21dc840892709a68299f6fa71772ff85
Steven Bustillo <sb.sys.dev@gmail.com>
2024-04-20 21:26:31 +0000

    :rocket: - updated to deploy on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        base: '/hostal-casa-liz-landing-page',
         ssr: {
             external: [],
         },
-        build: {
-            outDir: 'docs',
-        },
     },
+    site: 'https://sbsysdev.github.io',
+    base: 'hostal-casa-liz-landing-page',
+    outDir: 'docs',
 });
